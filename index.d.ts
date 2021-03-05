ea8c19fd023811fc755313fb7fdde9e4fc38e914
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2021-03-05 17:57:22 +0100

    Add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+export function addTime (angle: number, riseName: string, setName: string) {}
+export function getTimes (date: Date, lat: number, lng: number, height: number) {}
+
+export function getMoonPosition (date: Date, lat: number, lng: number) {}
+export function getMoonIllumination (date: Date) {}
+export function getMoonTimes (date: Date, lat: number, lng: number, inUTC: boolean) {}
