06360e37fcc46c35905cb0dcf5af5f628326f3cf
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2021-03-05 18:02:59 +0100

    types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,30 @@
-export function addTime (angle: number, riseName: string, setName: string) {}
-export function getTimes (date: Date, lat: number, lng: number, height: number) {}
+interface SunTimes {
+  solarNoon: Date;
+  nadir: Date;
+}
 
-export function getMoonPosition (date: Date, lat: number, lng: number) {}
-export function getMoonIllumination (date: Date) {}
-export function getMoonTimes (date: Date, lat: number, lng: number, inUTC: boolean) {}
+interface MoonTimes {
+  rise: Date;
+  set: Date;
+}
+
+interface MoonPosition {
+  azimuth: number;
+  altitude: number;
+  distance: number;
+  parallacticAngle: number;
+}
+
+interface MoonIllumination {
+  fraction: number;
+  phase: number;
+  angle: number;
+}
+
+export function addTime (angle: number, riseName: string, setName: string): void;
+export function getTimes (date: Date, lat: number, lng: number, height: number): Times;
+
+export function getMoonPosition (date: Date, lat: number, lng: number): MoonPosition;
+export function getMoonIllumination (date: Date): MoonIllumination;
+export function getMoonTimes (date: Date, lat: number, lng: number, inUTC: boolean): MoonTimes;
+
